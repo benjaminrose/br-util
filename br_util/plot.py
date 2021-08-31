7c60d3f9897e256f959bca9c3fefa7d939b041c0
Benjain Rose <benjamin.rose@duke.edu>
2021-08-31 10:00:08 -0400

    BREAKING: Add plot.new_figure & update arguments for plot.save_plot

--- br_util/plot.py
+++ br_util/plot.py
@@ -6,8 +6,21 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 
-def save_plot(filename, save):
-    if save:
+def new_figure(**kward):
+    """
+    passes all keyword arguments to `matplotlib.pyplot.subplots()`.
+
+    Returns:
+    -------
+    fig
+
+    ax
+    """
+    return plt.subplots(tight_layout=True, **kward)
+
+
+def save_plot(filename=""):
+    if filename:
         folder = Path("figures/")
         folder.mkdir(exist_ok=True)
         plt.savefig(folder / filename, bbox_inches="tight")
