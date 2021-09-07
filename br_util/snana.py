83741d698f1c5171c09b1d69145d7c5fc1027c2d
Benjain Rose <benjamin.rose@duke.edu>
2021-09-07 14:17:00 -0400

    Feat: add ability to user define index column
    * Also convert typical SNANA NaN values.

--- br_util/snana.py
+++ br_util/snana.py
@@ -3,9 +3,11 @@
 import pandas as pd
 
 
-def read_data(file_path, VERBOSE=False):
+def read_data(file_path, index_col=1, VERBOSE=False):
     with open(file_path, "r") as f:
-        data = pd.read_csv(f, sep="\s+", comment="#", index_col=1)
+        data = pd.read_csv(
+            f, sep="\s+", comment="#", na_values=[-9, -99], index_col=index_col
+        )
     data.drop(columns="VARNAMES:", inplace=True)
     if VERBOSE:
         print(data.describe())
