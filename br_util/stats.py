c35fa2472fe0bf4518910c3a8fb352181e9812b7
Benjain Rose <benjamin.rose@duke.edu>
2021-09-28 16:13:06 -0400

    Feat: add p2sigma

--- br_util/stats.py
+++ br_util/stats.py
@@ -1,6 +1,8 @@
 """stats.py"""
+from typing import List, Tuple
 
 import numpy as np
+from scipy.special import erfcinv
 
 
 def robust_scatter(x):
@@ -20,5 +22,9 @@ def robust_scatter(x):
     return 1.4826 * np.median(np.abs(x - np.median(x)))
 
 
-def p2sigma():
-    pass
+def p2sigma(p_value: Tuple[float], tails: int = 1) -> List[float]:
+    """Convert p-values to there corresponding sigma. Part of the clistats toolkit."""
+    # click returns variatic arguments (nargs=-1) as tuples
+    p_value = np.array(p_value)
+    sigmas = np.sqrt(2) * erfcinv(p_value * 2 / tails)
+    return sigmas
