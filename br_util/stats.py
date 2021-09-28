cfac47ad80cc56391d708aea3f171012b3a280b2
Benjain Rose <benjamin.rose@duke.edu>
2021-09-28 15:44:03 -0400

    Doc: update robust scatter docstring

--- br_util/stats.py
+++ br_util/stats.py
@@ -4,7 +4,19 @@ import numpy as np
 
 
 def robust_scatter(x):
-    "aka converting from MAD to sigma equivalent"
+    """aka MAD but converted to sigma equivalent.
+
+    1.4826 * median(|x - median(x)|)
+
+    Parameters
+    ----------
+    x: array_like
+        Data vector to calculated scatter of
+
+    Returns
+    -------
+    float
+    """
     return 1.4826 * np.median(np.abs(x - np.median(x)))
 
 
